6a92307874da1fdd826bc2de6d646fad590057e4
alpaBuddhabhatti <64379307+alpaBuddhabhatti@users.noreply.github.com>
2025-05-15 15:11:51 +0100

    Update and rename SampleInsertionData.sql to BusinessSourceDetails_Date.sql

---
+++ Real-world Demos/Real-world Demos/MatadataDriven_xlsx_csv_json_Demo/DB/BusinessSourceDetails_Date.sql
@@ -0,0 +1,9 @@
+INSERT INTO [config].[BusinessSourceDetails]
+    (
+      [Spec_Code]
+    , [Source_Location_Type]
+    , [Source_Location_Details]
+    , [Target_Location_Type]
+    , [Target_Location_Details]
+    , [Spec_Code_ID]
+    ) values('BO2', 'ONELAKE','BO2','BO2','BO2',10)
