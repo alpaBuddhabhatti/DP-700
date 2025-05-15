37a06bbe075ba0fd581dcdc6400059fb28cfb1ec
alpaBuddhabhatti <64379307+alpaBuddhabhatti@users.noreply.github.com>
2025-05-15 15:52:47 +0100

    Update and rename BusinessSourceDetails_Data.sql to 3_BusinessSourceDetails_Data.sql

---
+++ Real-world Demos/Real-world Demos/MatadataDriven_xlsx_csv_json_Demo/DB/3_BusinessSourceDetails_Data.sql
@@ -0,0 +1,14 @@
+INSERT INTO [config].[BusinessSourceDetails] (
+    [Spec_Code],
+    [Source_Location_Type],
+    [Source_Location_Details],
+    [Target_Location_Type],
+    [Target_Location_Details],
+    [Spec_Code_ID]
+)
+VALUES
+-- Record 1
+('BO3', 'OneLake', 'BO3', 'BO3', 'BO3', 3),
+
+-- Record 2
+('BO2', 'ONELAKE', 'BO2', 'BO2', 'BO2', 3);
